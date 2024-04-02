379cb59688a952b3d614e5247a9b2a505b5617da
Romain <romain.braud@e-artsupe.net>
2024-04-02 11:48:30 +0100

    instalation input systeme + mouvement

---
+++ Plateformer2D_Fun/Assets/Scripts/Mouvement/Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private int _speed;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    private Vector2 _value;
+
+    public void OnMouvement(InputAction.CallbackContext context)
+    {
+        _value = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 mouvement = new Vector3(_value.x, _value.y, 0);
+        mouvement.Normalize();
+        transform.Translate(_speed * mouvement * Time.deltaTime);
+    }
+}
