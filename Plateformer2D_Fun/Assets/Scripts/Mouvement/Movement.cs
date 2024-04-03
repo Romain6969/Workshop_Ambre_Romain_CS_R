6ca4a32618df74c6fcec207cc6b6d25b73669807
Romain <romain.braud@e-artsupe.net>
2024-04-03 15:52:52 +0100

    Debut Level Design + Fix turn avec perso

--- Plateformer2D_Fun/Assets/Scripts/Mouvement/Movement.cs
+++ Plateformer2D_Fun/Assets/Scripts/Mouvement/Movement.cs
@@ -5,6 +5,7 @@ public class Movement : MonoBehaviour
 {
     [field: SerializeField] public int Speed { get; set; }
     [field: SerializeField] public Rigidbody2D Rigidbody2D { get; set; }
+    private bool _isFacingRight = true;
     private Vector2 _value;
 
     public void OnMouvement(InputAction.CallbackContext context)
@@ -12,10 +13,27 @@ public class Movement : MonoBehaviour
         _value = context.ReadValue<Vector2>();
     }
 
+    public void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        Vector3 localscale = transform.localScale;
+        localscale.x *= -1f;
+        transform.localScale = localscale;
+    }
+
     private void FixedUpdate()
     {
         Vector3 mouvement = new Vector3(_value.x, _value.y, 0);
         mouvement.Normalize();
         transform.Translate(Speed * mouvement * Time.deltaTime);
+
+        if( !_isFacingRight && _value.x > 0)
+        {
+            Flip();
+        }
+        else if (_isFacingRight && _value.x < 0) 
+        {
+            Flip();
+        }
     }
 }
