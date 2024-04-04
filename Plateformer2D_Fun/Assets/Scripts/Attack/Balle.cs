78883de94dfb60d8adaa8785052a0d01fcac4ed4
MeguminFireFox <ambrehardy013@gmail.com>
2024-04-04 11:08:59 +0200

    vfx 1

--- Plateformer2D_Fun/Assets/Scripts/Attack/Balle.cs
+++ Plateformer2D_Fun/Assets/Scripts/Attack/Balle.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Balle : MonoBehaviour
@@ -6,7 +8,10 @@ public class Balle : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidBalle;
     [SerializeField] private float _force;
     [SerializeField] private GameObject _aim;
+    [SerializeField] private GameObject _vFXgraph;
     private Vector2 _direction;
+    private bool _isActive = false;
+    private float _time;
 
     private void Start()
     {
@@ -14,6 +19,21 @@ public class Balle : MonoBehaviour
         _direction = _aim.transform.position - transform.position;
         _direction.Normalize();
         _rigidBalle.AddForce(_direction * _force);
+    }
+
+    void Update()
+    {
+        if (_isActive == true)
+        {
+            _time += Time.deltaTime;
+
+            if (_time >= 0.2f)
+            {
+                _vFXgraph.SetActive(false);
+                _time = 0;
+                _isActive = false;
+            }
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -24,7 +44,9 @@ public class Balle : MonoBehaviour
         }
         else
         {
+            _vFXgraph.SetActive(true);
             _rigidBalle.AddForce(Vector2.up * (_force * 1.25f));
+            _isActive = true;
         }
     }
 }
