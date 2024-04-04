9f7d5af625da7186383234a5f97f71d8e2005896
Romain <romain.braud@e-artsupe.net>
2024-04-04 14:31:27 +0100

    Fin KnockBack

--- Plateformer2D_Fun/Assets/Scripts/HPPlayer.cs
+++ Plateformer2D_Fun/Assets/Scripts/HPPlayer.cs
@@ -1,30 +1,63 @@
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class HPPlayer : MonoBehaviour
 {
     [SerializeField] private HPEnemy _enemyPrefab;
+    [SerializeField] private GameObject _gameOverPanel;
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private float _force;
+    [SerializeField] private Movement _movement;
 
-    private float _invulnerability = 0;
+    private float _invulnerability = 0.5f;
     private float _hp = 5;
 
     void Update()
     {
+        _text.text = $"Life : {_hp}";
+
         _invulnerability += Time.deltaTime;
 
         if (_hp <= 0)
         {
             Destroy(gameObject);
+            _gameOverPanel.SetActive(true);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (_invulnerability >= 1.5f)
         {
             if (collision.gameObject.tag == "Enemy")
             {
                 _hp -= _enemyPrefab.Dammage;
+                _invulnerability = 0;
+
+                // Calculate Angle Between the collision point and the player
+                ContactPoint2D contactPoint = collision.GetContact(0);
+                Vector2 playerPosition = new Vector2(transform.position.x, 1f);
+                Vector2 dir = contactPoint.point - playerPosition;
+
+                // We then get the opposite (-Vector3) and normalize it
+                dir = -dir.normalized;
+
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                GetComponent<Rigidbody2D>().inertia = 0;
+
+                //If the problme is not resolved then lock controle key. "playerControles" is a public static boolean which you have declare in the player controller script with true. then in this script you have to enable it or disable it.like
+                _movement.PlayerController = false; //if its true player input buttons will work and vice versa.
+                Invoke("EnablePlayerControles", 1f); //if then amount of time is long then reduce it to the value you want.
+
+                // And finally we add force in the direction of dir and multiply it by force. 
+                // This will push back the player
+                GetComponent<Rigidbody2D>().AddForce(dir * _force, ForceMode2D.Impulse);
             }
         }
+    }
+    private void EnablePlayerControles()
+    {
+        _movement.PlayerController = true;
     }
 }
--- Plateformer2D_Fun/Assets/Scripts/Mouvement/Movement.cs
+++ Plateformer2D_Fun/Assets/Scripts/Mouvement/Movement.cs
@@ -7,6 +7,12 @@ public class Movement : MonoBehaviour
     [field: SerializeField] public Rigidbody2D Rigidbody2D { get; set; }
     private bool _isFacingRight = true;
     private Vector2 _value;
+    [field: SerializeField] public bool PlayerController { get; set; }
+
+    private void Start()
+    {
+        PlayerController = true;
+    }
 
     public void OnMouvement(InputAction.CallbackContext context)
     {
@@ -23,17 +29,21 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 mouvement = new Vector3(_value.x, _value.y, 0);
-        mouvement.Normalize();
-        transform.Translate(Speed * mouvement * Time.deltaTime);
+        if (PlayerController == true)
+        {
+            Vector3 mouvement = new Vector3(_value.x, _value.y, 0);
+            mouvement.Normalize();
+            transform.Translate(Speed * mouvement * Time.deltaTime);
 
-        if( !_isFacingRight && _value.x > 0)
-        {
-            Flip();
+            if (!_isFacingRight && _value.x > 0)
+            {
+                Flip();
+            }
+            else if (_isFacingRight && _value.x < 0)
+            {
+                Flip();
+            }
         }
-        else if (_isFacingRight && _value.x < 0) 
-        {
-            Flip();
-        }
+        
     }
 }
